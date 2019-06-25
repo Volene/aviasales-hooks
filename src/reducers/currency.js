c82ee1837b358dea76e1c3f7ce4822e3090cc1e9
Volene <44668364+Volene@users.noreply.github.com>
2019-06-25 20:21:23 +0300

    added default currency rate

--- src/reducers/currency.js
+++ src/reducers/currency.js
@@ -7,18 +7,26 @@ import {
 
 const initState = {
   activeCurr: "RUB",
-  rate:1,
+  rate: 1,
+  curr: {
+    EUR: 0.0139979759,
+    USD: 0.0159408949
+  }
 };
 export default (state = initState, action) => {
   switch (action.type) {
     case GET_CURRENCY_RATE_REQUEST:
       return { ...state };
     case GET_CURRENCY_RATE_SUCCESS:
-      return { ...state,curr: action.payload.rates };
+      return { ...state, curr: action.payload.rates };
     case GET_CURRENCY_RATE_FAILURE:
-      return { ...state,err:{}};
+      return { ...state, err: {} };
     case SET_CURRENCY:
-      return { ...state, activeCurr: action.payload, rate:state.curr[action.payload]||1 };
+      return {
+        ...state,
+        activeCurr: action.payload,
+        rate: state.curr[action.payload] || 1
+      };
     default:
       return state;
   }
